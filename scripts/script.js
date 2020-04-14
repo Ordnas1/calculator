a68c11abe277a368d08ccdce86dc9e0f34eaeebc
Ordnas1 <sandromarcelo.peirano@gmail.com>
2020-04-14 10:42:07 -0500

    Add operate function

--- scripts/script.js
+++ scripts/script.js
@@ -24,3 +24,22 @@ function divide(num1, num2) {
         return num1 / num2;
     }
 }
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+            break;
+        case '-':
+            return substract(num1, num2)
+            break;
+        case '*':
+            return multiply(num1, num2)
+            break;
+        case '/':
+            return divide(num1, num2)
+            break;
+        default:
+            return 'INVALID OPERATOR'
+    }
+}
